35a1c9a1531c48ac3d0316069144dc51caae5895
nguyenhuuthang-tdc <nguyenhuuthang12c8@gmail.com>
2023-03-07 10:55:11 +0700

    update accept client

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT_SERVER || 4000
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.URL_CLIENT);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
